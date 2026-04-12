7f601b3bb2577315ab1ebd715568f3d1e5553cef
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-12 22:34:34 +0700

    Update states.py

--- states.py
+++ states.py
@@ -1,5 +1,6 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class GameState(StatesGroup):
-    exploring = State()    # Mode aman (Muncul tombol arah)
-    in_combat = State()    # Mode tarung (Tombol hilang, harus ngetik)
+    exploring = State()    # Mode jelajah normal (Navigasi bebas)
+    in_combat = State()    # Mode pertarungan (Waktu terbatas, input jawaban)
+    traveling = State()    # Mode navigasi misi (Wajib mengikuti arah NPC 5x)
