5ee292101c7512f942e95b4f2c4bf64695522811
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-13 09:57:40 +0700

    Update bosses.py

--- game/entities/bosses.py
+++ game/entities/bosses.py
@@ -1,1 +1,16 @@
+"""
+Database Bos dan Mini-Bos Archivus
+"""
+import random
 
+BOSS_NAMES = [
+    "THE KEEPER", 
+    "JAMES MARCUS ECHO", 
+    "VOID OVERLORD", 
+    "THE FINAL ARCHIVIST", 
+    "ORPHAN OF THE ARCHIVES"
+]
+
+def get_random_boss():
+    """Mengambil nama bos acak"""
+    return random.choice(BOSS_NAMES)
