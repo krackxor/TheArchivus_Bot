13d3dea9e410271b87a229a19724c6616259428e
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-15 00:55:08 +0700

    Update states.py

--- states.py
+++ states.py
@@ -1,8 +1,9 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class GameState(StatesGroup):
-    exploring = State()    # State saat pemain sedang menjelajah jalan normal
-    traveling = State()    # State untuk misi manual (misal: disuruh NPC jalan 5 langkah)
-    in_combat = State()    # State saat pemain melawan monster/bos
-    in_quiz = State()      # State saat ditanya oleh NPC Quiz/Scholar
-    in_event = State()     # State baru untuk peti harta, NPC judi, dan kuil
+    exploring = State()       # State saat pemain sedang menjelajah jalan normal
+    traveling = State()       # State untuk misi manual (misal: disuruh NPC jalan 5 langkah)
+    in_combat = State()       # State saat pemain melawan monster/bos
+    in_quiz = State()         # State saat ditanya oleh NPC Quiz/Scholar
+    in_event = State()        # State untuk interaksi objek (peti harta, kuburan, patung, NPC judi)
+    in_rest_area = State()    # State saat pemain sedang beristirahat di kemah / berbelanja
