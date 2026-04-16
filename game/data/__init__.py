305025c364fc068d3ae022436ef70e50c3ebf506
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-17 03:14:10 +0700

    Update __init__.py

--- game/data/__init__.py
+++ game/data/__init__.py
@@ -1,6 +1,12 @@
 # game/data/__init__.py
 
+# Import data makhluk
 from .monster_data import MONSTER_POOL
 from .miniboss_data import MINI_BOSS_POOL
-from .mainboss_data import MAIN_BOSS_POOL  # Sesuaikan dengan nama file & variabelmu
+from .mainboss_data import MAIN_BOSS_POOL
+
+# Import data NPC
 from .npc_data import NPC_POOL, LORE_STORIES
+
+# Import data Narasi & Script (INI YANG KURANG)
+from .script import NARRATIVES, MONSTER_WARNINGS, DESPAIR_STEPS
