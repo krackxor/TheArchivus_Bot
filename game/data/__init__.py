4192ffe79c758c67ef77dee678640856c64d0289
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-17 03:16:12 +0700

    Add STALKING_MESSAGES to script imports

--- game/data/__init__.py
+++ game/data/__init__.py
@@ -1,12 +1,5 @@
-# game/data/__init__.py
-
-# Import data makhluk
 from .monster_data import MONSTER_POOL
 from .miniboss_data import MINI_BOSS_POOL
 from .mainboss_data import MAIN_BOSS_POOL
-
-# Import data NPC
 from .npc_data import NPC_POOL, LORE_STORIES
-
-# Import data Narasi & Script (INI YANG KURANG)
-from .script import NARRATIVES, MONSTER_WARNINGS, DESPAIR_STEPS
+from .script import NARRATIVES, MONSTER_WARNINGS, DESPAIR_STEPS, STALKING_MESSAGES
