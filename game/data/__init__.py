d394b015b4ed25c1384d857fe2b83296a2021169
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-17 02:51:51 +0700

    Update __init__.py

--- game/data/__init__.py
+++ game/data/__init__.py
@@ -1,5 +1,6 @@
 # game/data/__init__.py
+
 from .monster_data import MONSTER_POOL
 from .miniboss_data import MINI_BOSS_POOL
-from .boss_data import BOSS_DATA
-from .npc_data import NPC_DATA
+from .mainboss_data import MAIN_BOSS_POOL  # Sesuaikan dengan nama file & variabelmu
+from .npc_data import NPC_POOL, LORE_STORIES
