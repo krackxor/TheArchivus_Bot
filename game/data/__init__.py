3159e09daa45ed08ebfb628c4154c978398d1038
krackxor <77487814+krackxor@users.noreply.github.com>
2026-04-17 02:30:49 +0700

    Update __init__.py

--- game/data/__init__.py
+++ game/data/__init__.py
@@ -1,6 +1,5 @@
-
 # game/data/__init__.py
-
 from .monster_data import MONSTER_POOL
 from .miniboss_data import MINI_BOSS_POOL
-from .boss_data import BOSS_DATA # Sesuaikan dengan nama variabel di dalam filenya
+from .boss_data import BOSS_DATA
+from .npc_data import NPC_DATA
